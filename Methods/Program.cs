4dbba3030a4d1a8cc4138fab0dd2062f9aa7f3a3
Praveen <rompicharalaPraveen@gmail.com>
2021-09-18 16:03:47 +0530

    Methods

---
+++ Methods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Mymethod(string fname,int age)
+        {
+            Console.WriteLine(fname+ "\t" + age);
+        }
+        static void Main(string[] args)
+        {
+            // single time method
+            // Mymethod();
+
+            // Multiple times methods
+
+            /*  Mymethod();
+             Mymethod();
+             Mymethod(); */
+
+            // Passing Parameters in a method
+
+            /* Mymethod("Praveen");
+            Mymethod("Srinu");
+            Mymethod("Savitri"); */
+
+            // Default parameter
+
+            Mymethod("liam",5);
+            Mymethod("RR",6);
+
+        }
+    }
+}
